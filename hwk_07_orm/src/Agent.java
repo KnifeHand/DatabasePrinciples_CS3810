d195065097a212020348a35008bfe8de00a02677
Matt Hurt <mhurt@msudenver.edu>
2021-04-14 18:46:19 -0600

    Update project

--- hwk_07_orm/src/Agent.java
+++ hwk_07_orm/src/Agent.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Agent {
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Agents")
+public class Agent {
+
+    @Id
+    private int id;
+
+    private String name;
+
+    private int ouid;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getOuid() {
+        return ouid;
+    }
+
+    public void setOuid(int ouid) {
+    }
 }
